d51895f81fdaa3d77d0a570b4f92521d2fef1679
Gabriel Viana <gabrielalmeidavn@gmail.com>
2022-09-24 12:23:30 -0300

    criação do cadastro de usuários e adição do bootstrap

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Funcionario;
+use App\Models\Granja;
+use App\Models\Pesquisador;
+use App\Models\PesquisadorGranja;
+use App\Models\Proprietario;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rule;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +26,10 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        return view('auth.register', [
+            'granjas' => Granja::all(),
+            'pesquisadores' => Pesquisador::all()
+        ]);
     }
 
     /**
@@ -34,16 +43,66 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'nome' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'cpf' => ['required', 'string', 'min:11', 'max:11', 'unique:users'],
+            'perfil' => ['required', 'string', Rule::in(['Proprietário', 'Funcionário', 'Pesquisador'])],
+            'senha' => ['required']
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'nome' => $request->nome,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'cpf' => $request->cpf,
+            'senha' => Hash::make($request->senha),
         ]);
+
+        if ($request->perfil == 'Proprietário') {
+            $request->validate([
+                'email_comercial' => ['required', 'string', 'email', 'max:255', 'unique:proprietarios'],
+                'codigo_licenca' => ['required', 'string', 'unique:proprietarios']
+            ]);
+            Proprietario::create([
+                'email_comercial' => $request->email_comercial,
+                'codigo_licenca' => $request->codigo_licenca,
+                'id_usuario' => $user->id
+            ]);
+        } else if ($request->perfil == 'Funcionário') {
+            $request->validate([
+                'regime_contratacao' => ['required', 'string', 'max:255'],
+                'salario' => ['required', 'number']
+            ]);
+            try {
+                Funcionario::create([
+                    'regime_contratacao' => $request->regime_contratacao,
+                    'salario' => $request->salario,
+                    'id_granja' => Granja::findOrFail($request->id_granja_funcionario),
+                    'id_usuario' => $user->id
+                ]);
+            } catch (ModelNotFoundException $e) {
+                return redirect()->back()->with('error', 'Granja não existe!');
+            }
+        } else if ($request->perfil == 'Pesquisador') {
+            $request->validate([
+                'universidade' => ['required', 'string', 'max:255'],
+                'salario' => ['required', 'number']
+            ]);
+            try {
+                $pesquisador = Pesquisador::create([
+                    'universidade' => $request->universidade,
+                    'id_pesquisador_supervisor' => Pesquisador::findOrFail($request->id_pesquisador_supervisor),
+                    'id_usuario' => $user->id
+                ]);
+                PesquisadorGranja::create([
+                    'id_granja' => Granja::findOrFail($request->id_granja_pesquisador),
+                    'id_pesquisador' => $pesquisador->id
+                ]);
+            } catch (ModelNotFoundException $e) {
+                return redirect()->back()->with('error', 'Pesquisador supervisor ou Granja não existe!');
+            }
+        } else {
+            return null;
+        }
 
         event(new Registered($user));
 
